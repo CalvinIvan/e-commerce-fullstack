3fafadd54ab5032ea74a86eda96ae0073c260001
Calvin <CalvinIvan@live.com>
2023-11-02 09:32:18 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        { hostname: "images.unsplash.com" },
-        { hostname: "lh3.googleusercontent.com" },
-      ],
-    },
-    experimental: {
-      serverActions: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "github.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@prisma/client";
+import Image from "next/image";
 import Link from "next/link";
 
 interface ProductCardProps {
@@ -9,9 +10,13 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <Link
       href={"/products/" + product.id}
-      className="card w-full bg-base-100 hover:shadow-xl"
+      className="card w-full bg-base-100 transition-shadow hover:shadow-xl"
     >
-      asdfas
+      <div className="card-body">
+        <h2 className="card-title">
+          <Image src={product.imageUrl} alt="" width={50} height={50} />
+        </h2>
+      </div>
     </Link>
   );
 }
