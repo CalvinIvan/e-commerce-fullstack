b7e4b515badfe584d1ba570c66e77ad73f530525
Calvin <CalvinIvan@live.com>
2023-11-03 11:24:46 -0400

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,9 @@
 import PriceTag from "@/components/pricetag";
 import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 
 interface ProductPageProps {
   params: {
@@ -9,30 +11,51 @@ interface ProductPageProps {
   };
 }
 
-export default async function ProductPage({
-  params: { id },
-}: ProductPageProps) {
+const getProduct = cache(async (id: string) => {
   const product = await prisma.product.findUnique({
     where: { id },
   });
   if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name,
+    description: product.description,
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
 
   return (
-    <div className="flex flex-col p-4 lg:flex-row">
-      <Image
-        src={product.imageUrl}
-        alt={product.name}
-        width={400}
-        height={500}
-        priority
-        className="rounded-lg shadow-xl"
-      />
-      <div className="flex flex-col p-4">
-        <h1 className="my-5 text-5xl font-bold"> {product.name}</h1>
+    <section className="min-h-screen">
+      <div className="flex flex-col p-4 lg:flex-row lg:items-center">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={400}
+          height={500}
+          priority
+          className="rounded-lg shadow-xl"
+        />
+        <div className="flex flex-col p-4">
+          <h1 className="my-5 text-5xl font-bold"> {product.name}</h1>
 
-        <PriceTag price={product.price} className="badge badge-secondary" />
-        <p>{product.description}</p>
+          <PriceTag
+            price={product.price}
+            className="badge badge-secondary my-2"
+          />
+          <p>{product.description}</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
