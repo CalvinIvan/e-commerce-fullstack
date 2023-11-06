4c4d097d3bb0cee0f0de6747e60365dfa4a871eb
Calvin <CalvinIvan@live.com>
2023-11-06 11:55:57 -0500

    Update AddToCartButton.tsx

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,14 +1,32 @@
 "use client";
+import { useTransition } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 interface AddToCartButtonProps {
   productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
 }
 
-export default function AddToCartButton({ productId }: AddToCartButtonProps) {
+export default function AddToCartButton({
+  productId,
+  incrementProductQuantity,
+}: AddToCartButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
   return (
-    <div className="flex items-center gap-2">
-      <button className="btn btn-secondary" onClick={() => {}}>
+    <div className="mt-4 flex items-center gap-2">
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          setSuccess(false);
+          startTransition(() => {
+            incrementProductQuantity(productId).then(() => {
+              setSuccess(true);
+            });
+          });
+        }}
+      >
         Add to cart{" "}
         <div className="text-2xl">
           <AiOutlineShoppingCart />
@@ -16,4 +34,7 @@ export default function AddToCartButton({ productId }: AddToCartButtonProps) {
       </button>
     </div>
   );
+}
+function useState(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.");
 }
