ad0cea6b00d957fe3751a3b9fa834ef5ddda6c11
Calvin <CalvinIvan@live.com>
2023-11-06 16:29:25 -0500

    Update CartEntry.tsx

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -3,6 +3,7 @@
 import { CartItemWithProducts } from "@/lib/db/cart";
 import { formatPrice } from "@/lib/format";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartEntryProps {
   cartItem: CartItemWithProducts;
@@ -13,23 +14,25 @@ export default function CartEntry({
 }: CartEntryProps) {
   return (
     <div className="card mt-2 bg-slate-50/40 shadow-md transition hover:scale-[1.02] hover:shadow-2xl">
-      <div className="card-body flex flex-row items-center">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          width={150}
-          height={150}
-          className="rounded-xl"
-        />
-        <div className="flex flex-col">
-          <span className="text-xl font-bold">{product.name}</span>
+      <Link href={"/products/" + product.id}>
+        <div className="card-body flex flex-row items-center">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={150}
+            height={150}
+            className="rounded-xl"
+          />
+          <div className="flex flex-col">
+            <span className="text-xl font-bold">{product.name}</span>
 
-          <span className="text-xl font-bold">
-            {formatPrice(product.price)}
-          </span>
-          <span className="text-xl font-bold">Quantity: {quantity}</span>
+            <span className="text-xl font-bold">
+              {formatPrice(product.price)}
+            </span>
+            <span className="text-xl font-bold">Quantity: {quantity}</span>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
